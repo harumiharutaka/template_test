5304f9efc139a57245c2820af581a4248111f186
Yasutaka Takeyama <harumiharutaka@gmail.com>
2023-11-30 23:37:30 +0900

    ドロワー作成

--- src/js/module/dropdown.js
+++ src/js/module/dropdown.js
@@ -4,166 +4,148 @@
 
 export function dropdown() {
 
-    const dropdowns = document.querySelectorAll('.js_dropdown');
-    dropdowns.forEach(function(dropdown, index) {
+    const dropdown = document.querySelector('.js_dropdown');
 
-        // 子メニューの開閉
-        const parentBtns = dropdown.querySelectorAll('.js_dropdown_link_parent');
-        parentBtns.forEach(function(parentBtn, index) {
-    
-            parentBtn.onclick = function(e) {
-    
-                const childActive = this.nextElementSibling.classList.contains('dropdown__child--active');
-                if (!childActive){
-    
-                    const childActives = dropdown.querySelectorAll('.dropdown__child--active');
-                    childActives.forEach(function(childActive, index) {
-                        childActive.classList.remove('dropdown__child--active');
-                    });
-    
-                    this.nextElementSibling.classList.add('dropdown__child--active');
-    
-                } else {
-    
-                    this.nextElementSibling.classList.remove('dropdown__child--active');
-    
-                }
-    
+    // 子メニューを全て閉じる関数
+    function childAllClose(){
+        const childActives = dropdown.querySelectorAll('.dropdown__child--active');
+        childActives.forEach(function(childActive, index) {
+            childActive.classList.remove('dropdown__child--active');
+        });
+    }
+
+    // 孫メニューを全て閉じる関数
+    function grandchildAllClose(){
+        const grandchildActives = dropdown.querySelectorAll('.dropdown__grandchild--active');
+        grandchildActives.forEach(function(grandchildActive, index) {
+            grandchildActive.classList.remove('dropdown__grandchild--active');
+        });
+    }
+
+    // 子メニューを展開するボタンの処理
+    const parentBtns = dropdown.querySelectorAll('.js_dropdown_link_parent');
+    parentBtns.forEach(function(parentBtn, index) {
+
+        parentBtn.onclick = function(e) {
+
+            const childActive = this.nextElementSibling.classList.contains('dropdown__child--active');
+            if (!childActive){
+
+                childAllClose();
+                this.nextElementSibling.classList.add('dropdown__child--active');
+
+            } else {
+
+                this.nextElementSibling.classList.remove('dropdown__child--active');
+
             }
 
-            parentBtn.addEventListener('mouseover', function (e) {
+        }
+
+        parentBtn.addEventListener('mouseover', function (e) {
+
+            const childActive = this.nextElementSibling.classList.contains('dropdown__child--active');
+            if (!childActive){
+
+                const childActives = dropdown.querySelectorAll('.dropdown__child--active');
+                childActives.forEach(function(childActive, index) {
+                    childActive.classList.remove('dropdown__child--active');
+                });
+
+                if(childActives.length !== 0){
+                    this.nextElementSibling.classList.add('dropdown__child--active');
+                }
+
+            }
+
+        });
+
+    });
+    
+    // 孫メニューを展開するボタンの処理
+    const childParentBtns = dropdown.querySelectorAll('.js_dropdown_child_parent');
+    childParentBtns.forEach(function(childParentBtn, index) {
+
+        childParentBtn.parentNode.addEventListener('mouseover', function (e) {
+            childParentBtn.nextElementSibling.classList.add('dropdown__grandchild--active');
+        });
+
+        childParentBtn.parentNode.addEventListener('mouseleave', function (e) {
+            childParentBtn.nextElementSibling.classList.remove('dropdown__grandchild--active');
+        });
+
+        childParentBtn.addEventListener("keydown", (e) => {
+
+            if (e.key == "Enter") {
+
+                const grandchildActive = childParentBtn.nextElementSibling.classList.contains('dropdown__grandchild--active');
+                if(!grandchildActive){
+    
+                    grandchildAllClose();
+                    childParentBtn.nextElementSibling.classList.add('dropdown__grandchild--active');
+    
+                } else {
+
+                    childParentBtn.nextElementSibling.classList.remove('dropdown__grandchild--active');
+    
+                }
+
+            }
+
+        });
+
+    });
+
+    // フォーカスの処理
+    const focusBtns = dropdown.querySelectorAll('a,button');
+    focusBtns.forEach(function(focusBtn, index) {
+
+        focusBtn.addEventListener('focus', function (e) {
+
+            const linkParentActive = this.classList.contains('js_dropdown_link_parent');
+            const childParentActive = this.classList.contains('js_dropdown_child_parent');
+            const childActive = this.closest('.dropdown__child--active');
+            const grandchildActive = this.closest('.dropdown__grandchild--active');
+            if (linkParentActive) {
 
                 const childActive = this.nextElementSibling.classList.contains('dropdown__child--active');
                 if (!childActive){
-    
-                    const childActives = dropdown.querySelectorAll('.dropdown__child--active');
-                    childActives.forEach(function(childActive, index) {
-                        childActive.classList.remove('dropdown__child--active');
-                    });
 
-                    if(childActives.length !== 0){
-                        this.nextElementSibling.classList.add('dropdown__child--active');
-                    }
+                    childAllClose();
 
                 }
 
-            });
-    
-        });
-        
-        // 孫メニューの開閉
-        const childParentBtns = dropdown.querySelectorAll('.js_dropdown_child_parent');
-        childParentBtns.forEach(function(childParentBtn, index) {
+            } else if  (childParentActive) {
 
-            childParentBtn.parentNode.addEventListener('mouseover', function (event) {
-                childParentBtn.nextElementSibling.classList.add('dropdown__grandchild--active');
-            }, false);
+                const grandchildActive = this.nextElementSibling.classList.contains('dropdown__grandchild--active');
+                if (!grandchildActive){
 
-            childParentBtn.parentNode.addEventListener('mouseleave', function (event) {
-                childParentBtn.nextElementSibling.classList.remove('dropdown__grandchild--active');
-            }, false);
-
-            childParentBtn.addEventListener("keydown", (e) => {
-
-                if (e.key == "Enter") {
-
-                    const grandChildActive = childParentBtn.nextElementSibling.classList.contains('dropdown__grandchild--active');
-                    if(!grandChildActive){
-        
-                        const grandChildActives = dropdown.querySelectorAll('.dropdown__grandchild--active');
-                        grandChildActives.forEach(function(grandChildActive, index) {
-                            grandChildActive.classList.remove('dropdown__grandchild--active');
-                        });
-        
-                        childParentBtn.nextElementSibling.classList.add('dropdown__grandchild--active');
-        
-                    } else {
-
-                        childParentBtn.nextElementSibling.classList.remove('dropdown__grandchild--active');
-        
-                    }
-    
-                }
-
-            });
-
-        });
-
-        // フォーカスの処理
-        const focusBtns = dropdown.querySelectorAll('a,button');
-        focusBtns.forEach(function(focusBtn, index) {
-
-            focusBtn.addEventListener('focus', function (e) {
-
-                const linkParentActive = this.classList.contains('js_dropdown_link_parent');
-                const childParentActive = this.classList.contains('js_dropdown_child_parent');
-                const childActive = this.closest('.dropdown__child--active');
-                const grandchildActive = this.closest('.dropdown__grandchild--active');
-                if (linkParentActive) {
-
-                    const childActive = this.nextElementSibling.classList.contains('dropdown__child--active');
-                    if (!childActive){
-
-                        const childActives = dropdown.querySelectorAll('.dropdown__child--active');
-                        childActives.forEach(function(childActive, index) {
-                            childActive.classList.remove('dropdown__child--active');
-                        });
-    
-                    }
-
-                } else if  (childParentActive) {
-
-                    const grandchildActive = this.nextElementSibling.classList.contains('dropdown__grandchild--active');
-                    if (!grandchildActive){
-
-                        const grandchildActives = dropdown.querySelectorAll('.dropdown__grandchild--active');
-                        grandchildActives.forEach(function(grandchildActive, index) {
-                            grandchildActive.classList.remove('dropdown__grandchild--active');
-                        });
-    
-                    }
-
-                } else if  (!childActive) {
-
-                    const childActives = dropdown.querySelectorAll('.dropdown__child--active');
-                    childActives.forEach(function(childActive, index) {
-                        childActive.classList.remove('dropdown__child--active');
-                    });
-            
-                    const grandchildActives = dropdown.querySelectorAll('.dropdown__grandchild--active');
-                    grandchildActives.forEach(function(grandchildActive, index) {
-                        grandchildActive.classList.remove('dropdown__grandchild--active');
-                    });
-
-                } else if (!grandchildActive) {
-
-                    const grandchildActives = dropdown.querySelectorAll('.dropdown__grandchild--active');
-                    grandchildActives.forEach(function(grandchildActive, index) {
-                        grandchildActive.classList.remove('dropdown__grandchild--active');
-                    });
+                    grandchildAllClose();
 
                 }
-            
-            }); 
 
-        });
-    
+            } else if  (!childActive) {
+
+                childAllClose();
+                grandchildAllClose();
+
+            } else if (!grandchildActive) {
+
+                grandchildAllClose();
+            }
+        
+        }); 
+
     });
     
-    // ドロップダウン範囲外をクリックで子メニューを閉じる
+    
+    // ドロップダウン範囲外をクリックで子＆孫メニューを閉じる処理
     document.addEventListener('click', (e) => {
         
         if (!e.target.closest('.js_dropdown')) {
     
-            const childActives = document.querySelectorAll('.dropdown__child--active');
-            childActives.forEach(function(childActive, index) {
-                childActive.classList.remove('dropdown__child--active');
-            });
-
-            const grandchildActives = document.querySelectorAll('.dropdown__grandchild--active');
-            grandchildActives.forEach(function(grandchildActive, index) {
-                grandchildActive.classList.remove('dropdown__grandchild--active');
-            });
+            childAllClose();
+            grandchildAllClose();
     
         }
 
@@ -174,15 +156,8 @@ export function dropdown() {
 
         if (window.matchMedia('(max-width:991.98px)').matches) {
     
-            const childActives = document.querySelectorAll('.dropdown__child--active');
-            childActives.forEach(function(childActive, index) {
-                childActive.classList.remove('dropdown__child--active');
-            });
-
-            const grandchildActives = document.querySelectorAll('.dropdown__grandchild--active');
-            grandchildActives.forEach(function(grandchildActive, index) {
-                grandchildActive.classList.remove('dropdown__grandchild--active');
-            });
+            childAllClose();
+            grandchildAllClose();
     
         }
     
--- src/js/module/hamburger-and-drawer.js
+++ src/js/module/hamburger-and-drawer.js
@@ -7,19 +7,23 @@ export function hamburgerAndDrawer() {
     const hamburger = document.querySelector('.js_header_hamburger');
     const drawer = document.querySelector('.js_drawer');
     const overlay = document.querySelector('.js_drawer-overlay');
-    const drawerWidth = document.querySelector('.js_drawer').clientWidth;
+    let drawerWidth = 0;
     let childWidth = 0;
     let grandchildWidth = 0;
     const duration = 300;
 
     //ドロワーを開く関数
     function drawerOpen(){
+        drawerWidth = drawer.getBoundingClientRect().width;
         drawer.style.transitionProperty = 'right';
         drawer.style.transitionDuration = duration + 'ms';
         hamburger.classList.add('header__hamburger--active');
-        drawer.style.right = drawerWidth + 'px';
+        drawer.classList.add('drawer--active')
+        drawer.style.right = drawerWidth - 1 + 'px';
         overlay.classList.add('drawer-overlay--active');
         window.globalFunction.bodyScrollStop();
+        document.querySelector('.wrapper').inert = true;
+        document.querySelector('.footer').inert = true;
         setTimeout(() => {
             drawer.style.removeProperty('transition-property');
             drawer.style.removeProperty('transition-duration');
@@ -28,12 +32,16 @@ export function hamburgerAndDrawer() {
     
     //ドロワーを閉じる関数
     function drawerClose(){
+        drawerWidth = drawer.getBoundingClientRect().width;
         drawer.style.transitionProperty = 'right';
         drawer.style.transitionDuration = duration + 'ms';
         hamburger.classList.remove('header__hamburger--active');
+        drawer.classList.remove('drawer--active')
         drawer.style.right = 0;
         overlay.classList.remove('drawer-overlay--active');
         window.globalFunction.bodyScrollStart();
+        document.querySelector('.wrapper').inert = false;
+        document.querySelector('.footer').inert = false;
         setTimeout(() => {
             drawer.style.removeProperty('transition-property');
             drawer.style.removeProperty('transition-duration');
@@ -42,20 +50,22 @@ export function hamburgerAndDrawer() {
 
     //子メニューを開く関数
     function childOpen(){
+        drawerWidth = drawer.getBoundingClientRect().width;
         drawer.style.transitionProperty = 'right';
         drawer.style.transitionDuration = duration + 'ms';
-        drawer.style.right = drawerWidth + childWidth + 'px';
+        drawer.style.right = drawerWidth + childWidth - 2 + 'px';
         setTimeout(() => {
             drawer.style.removeProperty('transition-property');
             drawer.style.removeProperty('transition-duration');
         }, duration);
     }
 
-    //子メニューを開く関数
+    //子メニューを閉じる関数
     function childClose(){
+        drawerWidth = drawer.getBoundingClientRect().width;
         drawer.style.transitionProperty = 'right';
         drawer.style.transitionDuration = duration + 'ms';
-        drawer.style.right = drawerWidth + 'px';
+        drawer.style.right = drawerWidth - 1 + 'px';
         setTimeout(() => {
             drawer.style.removeProperty('transition-property');
             drawer.style.removeProperty('transition-duration');
@@ -64,27 +74,46 @@ export function hamburgerAndDrawer() {
 
     //孫メニューを開く関数
     function grandchildOpen(){
+        drawerWidth = drawer.getBoundingClientRect().width;
         drawer.style.transitionProperty = 'right';
         drawer.style.transitionDuration = duration + 'ms';
-        drawer.style.right = drawerWidth + childWidth + grandchildWidth + 'px';
+        drawer.style.right = drawerWidth + childWidth + grandchildWidth - 3 + 'px';
         setTimeout(() => {
             drawer.style.removeProperty('transition-property');
             drawer.style.removeProperty('transition-duration');
         }, duration);
     }
 
-    //孫メニューを開く関数
+    //孫メニューを閉じる関数
     function grandchildClose(){
+        drawerWidth = drawer.getBoundingClientRect().width;
         drawer.style.transitionProperty = 'right';
         drawer.style.transitionDuration = duration + 'ms';
-        drawer.style.right = drawerWidth + childWidth + 'px';
+        drawer.style.right = drawerWidth + childWidth - 2 + 'px';
         setTimeout(() => {
             drawer.style.removeProperty('transition-property');
             drawer.style.removeProperty('transition-duration');
         }, duration);
     } 
  
-    //ハンバーガークリックの処理
+    // 子メニューを全て閉じる関数
+    function childAllClose(){
+        const childActives = drawer.querySelectorAll('.drawer__child--active');
+        childActives.forEach(function(childActive, index) {
+            childActive.classList.remove('drawer__child--active');
+        });
+    }
+
+    // 孫メニューを全て閉じる関数
+    function grandchildAllClose(){
+        const grandchildActives = drawer.querySelectorAll('.drawer__grandchild--active');
+        grandchildActives.forEach(function(grandchildActive, index) {
+            grandchildActive.classList.remove('drawer__grandchild--active');
+        });
+    }
+
+
+    //ハンバーガーボタンの処理
     hamburger.onclick = function() {
         const active = this.classList.contains('header__hamburger--active');
     
@@ -94,45 +123,34 @@ export function hamburgerAndDrawer() {
 
             drawerClose();
             setTimeout(() => {
-                const childActives = drawer.querySelectorAll('.drawer__child--active');
-                childActives.forEach(function(childActive, index) {
-                    childActive.classList.remove('drawer__child--active');
-                });
-                const grandchildActives = drawer.querySelectorAll('.drawer__grandchild--active');
-                grandchildActives.forEach(function(grandchildActive, index) {
-                    grandchildActive.classList.remove('drawer__grandchild--active');
-                });
+                childAllClose();
+                grandchildAllClose();
             }, duration);
 
         }
     
     } 
 
-    // 子メニューの処理
+    // 子メニューを展開するボタンの処理
     const parentBtns = drawer.querySelectorAll('.js_drawer_link_parent');
     parentBtns.forEach(function(parentBtn, index) {
 
         parentBtn.onclick = function(e) {
 
             const childActive = this.nextElementSibling.classList.contains('drawer__child--active');
-            childWidth = this.nextElementSibling.clientWidth;
+            childWidth = this.nextElementSibling.getBoundingClientRect().width;
 
             if (!childActive){
 
-                const childActives = drawer.querySelectorAll('.drawer__child--active');
-                childActives.forEach(function(childActive, index) {
-                    childActive.classList.remove('drawer__child--active');
-                });
+                childAllClose();
+                grandchildAllClose();
                 this.nextElementSibling.classList.add('drawer__child--active');
                 childOpen();
 
             } else {
 
                 setTimeout(() => {
-                    const childActives = drawer.querySelectorAll('.drawer__child--active');
-                    childActives.forEach(function(childActive, index) {
-                        childActive.classList.remove('drawer__child--active');
-                    });
+                    childAllClose();
                 }, duration);
                 childClose();
 
@@ -142,31 +160,41 @@ export function hamburgerAndDrawer() {
 
     });
 
-    // 孫メニューの処理
+    // 子メニューを閉じるボタンの処理
+    const childCloseBtns = drawer.querySelectorAll('.js_drawer_child_close');
+    childCloseBtns.forEach(function(childCloseBtn, index) {
+
+        childCloseBtn.onclick = function(e) {
+            
+            setTimeout(() => {
+                childAllClose();
+                grandchildAllClose();
+            }, duration);
+            childClose();
+            
+        }
+
+    });
+
+    // 孫メニューを展開するボタンの処理
     const childParentBtns = drawer.querySelectorAll('.js_drawer_child_parent');
     childParentBtns.forEach(function(childParentBtn, index) {
 
         childParentBtn.onclick = function(e) {
 
             const grandchildActive = this.nextElementSibling.classList.contains('drawer__grandchild--active');
-            grandchildWidth = this.nextElementSibling.clientWidth;
+            grandchildWidth = this.nextElementSibling.getBoundingClientRect().width;
 
             if (!grandchildActive){
 
-                const grandchildActives = drawer.querySelectorAll('.drawer__grandchild--active');
-                grandchildActives.forEach(function(grandchildActive, index) {
-                    grandchildActive.classList.remove('drawer__grandchild--active');
-                });
+                grandchildAllClose();
                 this.nextElementSibling.classList.add('drawer__grandchild--active');
                 grandchildOpen();
 
             } else {
 
                 setTimeout(() => {
-                    const grandchildActives = drawer.querySelectorAll('.drawer__grandchild--active');
-                    grandchildActives.forEach(function(grandchildActive, index) {
-                        grandchildActive.classList.remove('drawer__grandchild--active');
-                    });
+                    grandchildAllClose();
                 }, duration);
                 grandchildClose();
 
@@ -175,14 +203,30 @@ export function hamburgerAndDrawer() {
         }
 
     });
+
+    // 孫メニューを閉じるボタンの処理
+    const grandchildCloseBtns = drawer.querySelectorAll('.js_drawer_grandchild_close');
+    grandchildCloseBtns.forEach(function(grandchildCloseBtn, index) {
+
+        grandchildCloseBtn.onclick = function(e) {
+            
+            setTimeout(() => {
+                grandchildAllClose();
+            }, duration);
+            grandchildClose();
+            
+        }
+
+    });
        
-    
     //オーバーレイクリックの処理
     overlay.onclick = function() {
 
         const active = this.classList.contains('drawer-overlay--active');
         if(active){
             drawerClose();
+            childAllClose();
+            grandchildAllClose();
         }
     
     }
@@ -195,6 +239,8 @@ export function hamburgerAndDrawer() {
             const active = hamburger.classList.contains('header__hamburger--active');
             if(active){
                 drawerClose();
+                childAllClose();
+                grandchildAllClose();
             }
     
         }
