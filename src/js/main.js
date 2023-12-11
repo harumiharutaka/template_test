ab00cacd0cfa6f446dbf04baefff8d3731acd7c7
y.takeyama <y.takeyama@nttcoms.com>
2023-12-11 11:55:36 +0900

    微修正

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,37 @@
 'use strict';
 
 //bodyのスクロール制御
-import { bodyScroll } from "./module/bodyScroll";
+import { bodyScroll } from './module/bodyScroll';
 bodyScroll();
 
+//ドロップダウン
+import { dropdown } from './module/dropdown';
+dropdown();
+
 //ハンバーガー＆ドロワー
-import { hamburgerAndDrawer } from "./module/hamburger-and-drawer";
+import { hamburgerAndDrawer } from './module/hamburger-and-drawer';
 hamburgerAndDrawer();
 
 //ページトップ
-import { pagetop } from "./module/pagetop";
+import { pagetop } from './module/pagetop';
 pagetop();
 
-//ローディング
-import { loading } from "./module/loading";
-loading();
-
-//アコーディオン
-import { accordion } from "./module/accordion";
-accordion();
-
 //タブ
-import { tab } from "./module/tab";
+import { tab } from './module/tab';
 tab();
 
+//アコーディオン
+import { accordion } from './module/accordion';
+accordion();
+
 //モーダル
-import { modal } from "./module/modal";
+import { modal } from './module/modal';
 modal();
 
-//ドロップダウン
-import { dropdown } from "./module/dropdown";
-dropdown();
+//ローディング
+import { loading } from './module/loading';
+loading();
 
 //プラグイン
-import { plugin } from "./module/plugin";
+import { plugin } from './module/plugin';
 plugin();
